23cfd4371af3dd897502ab1317ca77f73ffa8275
D3 <118836504+d3ltah@users.noreply.github.com>
2023-07-11 20:32:41 +0000

    Fixed SVGs sometimes not loading

--- public/global.js
+++ public/global.js
@@ -18,25 +18,4 @@ fetch("./components/footer.html")
   .then(res => res.text())
   .then(html => footerWrapper.innerHTML = twemoji.parse(html));
 
-
-// insert svg logos where needed
-var wcLogos = document.getElementsByClassName("wclogo");
-var d3Logos = document.getElementsByClassName("d3logo");
-
-fetch("./components/wcLogo-svg.txt")
-  .then(res => res.text())
-  .then(svgContent => {
-    for (var i = 0; i < wcLogos.length; i++) {
-      wcLogos[i].innerHTML = svgContent;
-    }
-  });
-
-fetch("./components/d3Logo-svg.txt")
-  .then(res => res.text())
-  .then(svgContent => {
-    for (var i = 0; i < d3Logos.length; i++) {
-      d3Logos[i].innerHTML = svgContent;
-    }
-  });
-
 twemoji.parse(document.body);
