2cc75ec7fca97058d814328d8b35de9c613ea47c
d3ltah <deltah404@gmail.com>
2023-07-26 14:40:28 +0100

    Fix Home button in index redirecting to 404 page

--- public/pages/pages.js
+++ public/pages/pages.js
@@ -6,6 +6,11 @@ var indexContainer = document.getElementById("index-container");
 
 for (page in listedPages) {
     var indexElement = document.createElement("h2");
-    indexElement.innerHTML = `<a href="/${listedPages[page]}">${listedPages[page].toUpperCase()}</a>`;
+
+    if (listedPages[page] != "home") {
+        indexElement.innerHTML = `<a href="/${listedPages[page]}">${listedPages[page].toUpperCase()}</a>`;
+    } else {
+        indexElement.innerHTML = `<a href="/">HOME</a>`;
+    }
     indexContainer.appendChild(indexElement);
 }
