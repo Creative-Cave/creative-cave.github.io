7513b9bdd61c24296c9178ea7cd5add5dce22ec9
d3ltah <deltah404@gmail.com>
2023-10-02 22:07:03 +0100

    Library card animations

--- public/library/library.js
+++ public/library/library.js
@@ -1,4 +1,5 @@
-var url = "https://raw.githubusercontent.com/Writers-Cave/data/main/library/library.json";
+var url =
+	"https://raw.githubusercontent.com/Writers-Cave/data/main/library/library.json";
 const libraryCardTemplate = `
 <div>
     <p class="library-card-title">{title}</p>
@@ -6,41 +7,54 @@ const libraryCardTemplate = `
 </div>
 <div>
     <p class="library-card-genre">{genre}</p>
-</div>`
+</div>`;
+
+let cardContainer = document.getElementById("library-container");
 
 function createCard(workData) {
-    console.log(workData);
-    let element = document.createElement("a");
-    
-    element.href = workData.url;
-    element.target = "_blank";
-    element.rel = "noopener noreferrer";
+	console.log(workData);
+	let element = document.createElement("a");
 
-    element.classList += "library-card";
+	element.href = workData.url;
+	element.target = "_blank";
+	element.rel = "noopener noreferrer";
 
-    if (workData.genre == undefined) {
-        workData.genre = "Other";
-    }
+	element.classList += "library-card";
 
-    element.innerHTML = libraryCardTemplate.replace("{title}", workData.title).replace("{author}", workData.author).replace("{genre}", workData.genre);
+	if (workData.genre == undefined) {
+		workData.genre = "Other";
+	}
 
-    // element.style.backgroundImage = generateRandomGradients();
-    
-    return element;
+	element.innerHTML = libraryCardTemplate
+		.replace("{title}", workData.title)
+		.replace("{author}", workData.author)
+		.replace("{genre}", workData.genre);
+
+	// element.style.backgroundImage = generateRandomGradients();
+
+	return element;
 }
 
 async function getLibrary() {
-    var library;
-    const response = await fetch(url);
-    library = await response.json();
+	var library;
+	var cards;
+	const response = await fetch(url);
+	library = await response.json();
 
-    console.log(library.library);
-    // updateSubmissionCounter(Object.keys(library.submissions).length);
+	// updateSubmissionCounter(Object.keys(library.submissions).length);
 
-    //for (workID in shuffle(library.library)) {
-    for (workID in library.library) {
-        document.getElementById("library-container").appendChild(createCard(library.library[workID]));
-    }
+	//for (workID in shuffle(library.library)) {
+	for (let i = 0; i < Object.keys(library.library).length; i++) {
+        setTimeout(() => {
+            cardContainer.appendChild(createCard(library.library[`id${i+1}`]));
+        }, 100 * i);
+	}
+
+	return cards;
 }
 
-getLibrary();
+let cards = getLibrary();
+
+console.log(cards);
+
+
